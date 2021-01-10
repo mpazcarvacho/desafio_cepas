34d9d35cbd19baf015ca6fb183960f46e525887d
mariapaz <mpazcarvacho@gmail.com>
2021-01-10 20:23:44 -0300

    Bootstrap and navbar added

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,4 +1,13 @@
 class Wine < ApplicationRecord
   has_many :assemblies
   has_many :strains, through: :assemblies, dependent: :destroy
+
+  # def strains_details
+    
+  #   details = assemblies.map do |assembly|
+  #     "#{assembly.strain_name}: #{assembly.percentage}%"
+  #   end
+  #   details.sort.join(", ")
+  # end
+
 end
